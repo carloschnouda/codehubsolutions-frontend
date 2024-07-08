bb0aa248ab61634c9cb131f28a65a349f3f19835
carloschnouda <carlos.chnouda@gmail.com>
2024-07-08 09:35:46 +0300

    Add cache control no cache

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,7 +9,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 export const fetchCache = 'force-no-store'
 
 async function getGeneralSettings() {
-  const res = axios.get('https://admin.codehubsolutions.net/api/general');
+  const res = axios.get('https://admin.codehubsolutions.net/api/general', { headers: { "Cache-Control": "no-cache" } });
   return (await res).data;
 }
 
@@ -25,7 +25,7 @@ export let metadata = {
     siteName: 'CodeHubSolutions',
     images: 'https://codehub-solutions.vercel.app/og-image.png',
   },
-  
+
 };
 
 //Favicon
