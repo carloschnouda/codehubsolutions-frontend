5f8fb5b15eb3471fe93c42ad8c1881bfa7349ed1
carloschnouda <carlos.chnouda@gmail.com>
2024-08-12 21:56:29 +0300

    Remove purchase button

--- src/components/services/ServicesCard.jsx
+++ src/components/services/ServicesCard.jsx
@@ -14,10 +14,6 @@ function ServicesCard({ service }) {
                     </div>
                     <div className={styles.cardDescription + ` text-lg text-[#00004b] proxima-nova-regular`} dangerouslySetInnerHTML={{ __html: service?.description }} ></div>
                 </div>
-
-                <div className=" text-center text-md proxima-nova-regular  bg-[#0000ee] text-white hover:bg-transparent hover:text-[#0000ee] transition-all animation-duration-300 border cursor-pointer border-[#0000ee] py-2 px-3 mt-4  mb-3 rounded-full ticket">
-                    Purchase
-                </div>
             </div>
         </>
     )
